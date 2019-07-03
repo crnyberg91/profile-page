48c68dc5acdbb957ed6072598576d2ce6834ff0d
Chris Nyberg <chrisn83@gmail.com>
2019-07-03 15:32:44 -0400

    footer remade, background fixed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import "./style.css";
+import { Navbar, Container } from "react-bootstrap";
 
 function Footer() {
     return (
-        <footer class="about-footer">
-            <h4>Contact Me</h4>
-            <a target="_blank" href="https://github.com/crnyberg91">
-                <img id="githubIMG" src="assets/images/github.png" />
-            </a>
-            <a target="_blank" href="https://www.linkedin.com/in/christopher-nyberg-099350115/">
-                <img id="githubIMG" src="assets/images/linkedin.png" />
-            </a>
-            <h5>Copyright©</h5>
-        </footer>
+        <Navbar expand="lg" variant="light" bg="light" sticky="bottom" className="dusty-grass-gradient footer">
+            <Container>
+                <Navbar.Brand href="#">Navbar</Navbar.Brand>
+            </Container>
+        </Navbar>
     )
 }
 

a2027132ce790187168f65fedf7ad3558aba6680
Chris Nyberg <chrisn83@gmail.com>
2019-07-04 16:25:06 -0400

    fixed horizontal scroll problem

--- src/components/MainContentContainer/index.js
+++ src/components/MainContentContainer/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import About from "../About";
-import PortfolioCard from "../PortfolioCard";
 import Container from "react-bootstrap/Container";
 // import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
